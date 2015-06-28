6c63ff37fa68682472e9193bb0acee4d73a6688b
spq24 <spq2461@gmail.com>
2015-06-28 03:59:53 +0000

    removing unneeded migration

--- db/migrate/20150521184905_reallyaddencryptedcolumns.rb
+++
@@ -1,7 +0,0 @@
-class Reallyaddencryptedcolumns < ActiveRecord::Migration
-  def change
-  	add_column :companies, :encrypted_publishable_key, :string
-  	add_column :companies, :encrypted_uid, :string
-  	add_column :companies, :encrypted_access_code, :string
-  end
-end
